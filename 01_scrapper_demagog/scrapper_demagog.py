a8002036ff119004d0fd3b4665a23604dc919c02
Marek <marek.brynda96@gmail.com>
2022-05-06 15:14:36 +0200

    extraction of features

--- 01_scrapper_demagog/scrapper_demagog.py
+++ 01_scrapper_demagog/scrapper_demagog.py
@@ -3,6 +3,7 @@ import requests
 
 import time
 import numpy as np
+import pandas as pd
 
 from tqdm import tqdm
 
@@ -13,10 +14,14 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 n_pages = int(soup.findAll("a", class_="page-numbers")[1].text)
 
-txt_list = []
-assestment_list = []
-txt_list = []
-assestment_list = []
+if False:
+    df_in = pd.read_csv('../datasets/demagog.csv', sep=';')
+    txt_list = df_in['text'].values.tolist()
+    assestment_list = df_in['assestment'].values.tolist()
+else:
+    txt_list = []
+    assestment_list = []
+
 for i in range(1,n_pages+1):
     
     page=requests.get(f'{demagog_statements_url}{i}')
@@ -27,19 +32,27 @@ for i in range(1,n_pages+1):
                   position=0
                  ):
         statement_url = s.find('a').get("href")
-
-        page_statement = requests.get(statement_url)
-        soup_statement = BeautifulSoup(page_statement.content, 'html.parser')
-
-        txt = soup_statement.find('blockquote', class_='hyphenate target-blank twitter-tweet') \
-                .find('p').text
-
-        assestment = soup_statement.find_all('p', {"class": "ocena"})[0].text
         
-        txt_list.append(txt)
-        assestment_list.append(assestment)
-        
-        time.sleep(np.random.randint(5)/5)
+        try:
+            page_statement = requests.get(statement_url)
+            soup_statement = BeautifulSoup(page_statement.content, 'html.parser')
+
+            txt = soup_statement.find('blockquote', class_='hyphenate target-blank twitter-tweet') \
+                    .find('p').text
+
+            assestment = soup_statement.find_all('p', {"class": "ocena"})[0].text
+
+            txt_list.append(txt)
+            assestment_list.append(assestment)
+
+            time.sleep(1)
+        except Exception as e: 
+            print(e)
+            print(statement_url)
+
+    df_out = pd.DataFrame({'assestment' : assestment_list, 'text' : txt_list})
+    df_out.to_csv('../datasets/demagog.csv', sep=';', index=False)
+    
     
 df_out = pd.DataFrame({'assestment' : assestment_list, 'text' : txt_list})
 df_out.to_csv('../datasets/demagog.csv', sep=';', index=False)

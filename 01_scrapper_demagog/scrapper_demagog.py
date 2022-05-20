b3ce2956194ad692c6e2697b8d7ef5ec267bbeb9
Marek <marek.brynda96@gmail.com>
2022-05-20 11:24:18 +0200

    update scrapper

--- 01_scrapper_demagog/scrapper_demagog.py
+++ 01_scrapper_demagog/scrapper_demagog.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 from tqdm import tqdm
 
+import unicodedata
+
 demagog_statements_url = 'https://demagog.org.pl/wypowiedzi/page/'
 page = requests.get(f'{demagog_statements_url}1')
 
@@ -14,13 +16,20 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 n_pages = int(soup.findAll("a", class_="page-numbers")[1].text)
 
-if False:
-    df_in = pd.read_csv('../datasets/demagog.csv', sep=';')
-    txt_list = df_in['text'].values.tolist()
-    assestment_list = df_in['assestment'].values.tolist()
-else:
-    txt_list = []
-    assestment_list = []
+# if False:
+#     df_in = pd.read_csv('../datasets/demagog.csv', sep=';')
+#     txt_list = df_in['text'].values.tolist()
+#     assestment_list = df_in['assestment'].values.tolist()
+# else:
+#     txt_list = []
+#     assestment_list = []
+
+txt_list = []
+assestment_list = []
+author_list = []
+source_list = []
+
+list_wrong_url = []
 
 for i in range(1,n_pages+1):
     
@@ -37,22 +46,36 @@ for i in range(1,n_pages+1):
             page_statement = requests.get(statement_url)
             soup_statement = BeautifulSoup(page_statement.content, 'html.parser')
 
-            txt = soup_statement.find('blockquote', class_='hyphenate target-blank twitter-tweet') \
-                    .find('p').text
+            txt = unicodedata.normalize("NFKD", 
+                                        soup_statement.find('blockquote', class_='hyphenate target-blank twitter-tweet') \
+                    .find('p').text)
 
-            assestment = soup_statement.find_all('p', {"class": "ocena"})[0].text
+            assestment = unicodedata.normalize("NFKD", 
+                                               soup_statement.find_all('p', {"class": "ocena"})[0].text)
+            author = unicodedata.normalize("NFKD", 
+                                           soup_statement.find('div', class_='h2 person-name mt-0 count-text').find('a').text)
+            if soup_statement.find('div', class_='w-100 text-right date-content target-blank'):
+            	source = unicodedata.normalize("NFKD", 
+                                           soup_statement.find('div', class_='w-100 text-right date-content target-blank').find('p').text)
+            else:
+            	source = ['wypowiedź']
 
             txt_list.append(txt)
             assestment_list.append(assestment)
-
-            time.sleep(1)
+            author_list.append(author)
+            source_list.append(source)
+        
+            #time.sleep(0.1)
+            
         except Exception as e: 
             print(e)
-            print(statement_url)
+            print(statement_url)    
+            list_wrong_url.append(statement_url)
 
-    df_out = pd.DataFrame({'assestment' : assestment_list, 'text' : txt_list})
+
+    df_out = pd.DataFrame({'assestment' : assestment_list, 'text' : txt_list, 'author' : author_list, 'source' : source_list})
     df_out.to_csv('../datasets/demagog.csv', sep=';', index=False)
     
     
-df_out = pd.DataFrame({'assestment' : assestment_list, 'text' : txt_list})
+df_out = pd.DataFrame({'assestment' : assestment_list, 'text' : txt_list, 'author' : author_list, 'source' : source_list})
 df_out.to_csv('../datasets/demagog.csv', sep=';', index=False)

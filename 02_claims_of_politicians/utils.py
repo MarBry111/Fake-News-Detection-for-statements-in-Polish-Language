9285c6043d3366c5b2cc1cd3ccf7d3a82244f007
Marek <marek.brynda96@gmail.com>
2022-10-01 14:57:42 +0200

    deletign old notebooks, and moving them to cleaner version of new ones for my data and czech one

--- 02_claims_of_politicians/utils.py
+++ 02_claims_of_politicians/utils.py
@@ -30,6 +30,35 @@ spell = Speller('pl')
 
 stopwords = nlp_core.Defaults.stop_words
 
+def tokenize(txt, nlp_core=nlp_core, stopwords=stopwords):
+    txt = (txt.replace('\n', ' ')
+           .replace('ą', 'ą')
+           .replace('ć', 'ć')
+           .replace('ę', 'ę')
+           .replace('ń', 'ń')
+           .replace('ó', 'ó')
+           .replace('ś', 'ś')
+           .replace('ź', 'ź')
+           .replace('ż', 'ż')
+           .replace('  ', ' '))
+
+    doc = nlp_core(txt)
+    
+    words = [
+        token.lemma_.lower()
+        for token in doc 
+        if 
+            not token.is_stop 
+            and not token.is_punct 
+            and not token.is_stop 
+            and not token.is_digit
+            and token.text != ' '
+            and token.lemma_ not in stopwords
+            and len(token.text) > 2 ]
+    
+    return words
+
+
 # text cleaning
 def clean_przyp(txt):
     if txt != txt:
@@ -94,6 +123,7 @@ def extract_features(txt, nlp_core=nlp_core):
     out_dict['sentiment_avg'] = np.mean(sentiments_list)
 
     txt_pos = []
+    txt_word = []
     for token in doc:
         if token.pos_ not in ['SPACE', 'PUNCT']:
             lemmas_list.append(token.lemma_)
@@ -102,18 +132,17 @@ def extract_features(txt, nlp_core=nlp_core):
             if corrected != token.text:
                 error_n += 1
 
-        if token.pos_ == 'ADJ': 
-            adj_n += 1
-        elif token.pos_ == 'ADV':
-            adv_n += 1
-        elif token.pos_ == 'NOUN':
-            noun_n += 1
+        # if token.pos_ == 'ADJ': 
+        #     adj_n += 1
+        # elif token.pos_ == 'ADV':
+        #     adv_n += 1
+        # elif token.pos_ == 'NOUN':
+        #     noun_n += 1
         
-        if token.text[0].isupper():
-            upper_s_n += 1
-        
-        txt_pos.append(token.pos_)
-        
+            if token.text[0].isupper():
+                upper_s_n += 1
+            
+            txt_pos.append(token.pos_)            
 
     for ent in doc.ents:
         ent_n += 1
@@ -125,9 +154,9 @@ def extract_features(txt, nlp_core=nlp_core):
     out_dict['uniq_lemm'] =  len(lemmas_list)
     out_dict['err'] =  error_n
     out_dict['net'] = ent_n
-    out_dict['ADJ'] = adj_n/len(tokens_list)
-    out_dict['ADV'] = adv_n/len(tokens_list)
-    out_dict['NOUN'] = noun_n/len(tokens_list)
+    # out_dict['ADJ'] = adj_n/len(tokens_list)
+    # out_dict['ADV'] = adv_n/len(tokens_list)
+    # out_dict['NOUN'] = noun_n/len(tokens_list)
     
     out_dict['words_start_upper'] = upper_s_n
     out_dict['words_full_upper'] = upper_f_n
@@ -138,33 +167,7 @@ def extract_features(txt, nlp_core=nlp_core):
     out_dict['chars'] = len(txt)
 
     out_dict['TEXT_POS'] = txt_pos
+
+    out_dict['TEXT_WORD'] = tokenize(txt)
     
     return out_dict
-
-
-def tokenize(txt, nlp_core=nlp_core, stopwords=stopwords):
-    txt = (txt.replace('\n', ' ')
-           .replace('ą', 'ą')
-           .replace('ć', 'ć')
-           .replace('ę', 'ę')
-           .replace('ń', 'ń')
-           .replace('ó', 'ó')
-           .replace('ś', 'ś')
-           .replace('ź', 'ź')
-           .replace('ż', 'ż'))
-
-    doc = nlp_core(txt)
-    
-    words = [
-        token.lemma_.lower()
-        for token in doc 
-        if 
-            not token.is_stop 
-            and not token.is_punct 
-            and not token.is_stop 
-            and not token.is_digit
-            and token.text != ' '
-            and token.lemma_ not in stopwords
-            and len(token.text) > 2 ]
-    
-    return words
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ The idea is simple - fact-checking websites can provide "claim" with label as "f
 - Data from getting_real_about_fake_news - https://www.kaggle.com/datasets/mrisdal/fake-news
 - Data from oko.press - data, courtesy of OKO.press team (https://oko.press/) 
 - Data from MagaCOV - https://github.com/UBC-NLP/megacov 
+- Data from liar - https://www.cs.ucsb.edu/~william/data/liar_dataset.zip
 
 ### Creation of weak supervised dataset
 Idea behind this approach was inspired by paper [Weakly Supervised Learning for Fake News Detection on Twitter](https://ieeexplore.ieee.org/document/8508520) where authors created training dataset by assuming that all posts from trustworthy page is truth and from troll website is fake. As the extension of this method could be used some metrics from [Computational Propaganda in  Poland:  False Amplifiers and the Digital Public  Sphere](https://blogs.oii.ox.ac.uk/politicalbots/wp-content/uploads/sites/89/2017/06/Comprop-Poland.pdf), where various methods have been used to identify troll accounts.
